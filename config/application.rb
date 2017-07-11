1b9335ea38cc8c0aa24db7f3a5c4898f7ece1338
yungpanko <jrjohns@mit.edu>
2017-07-11 12:15:09 -0400

    allow local access to 8080

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Flatironboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => 'http://localhost:8080',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
